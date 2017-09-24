80c5474fd6d1e6f9c3614f1e10752c291bd60f14
Ignas Grigaravicius <kickeris@gmail.com>
2017-09-24 15:50:28 +0100

    about models changed and migrated

--- about/models.py
+++ about/models.py
@@ -1,12 +1,22 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
+
 class Abou(models.Model):
-    name = models.CharField(max_length=254, default='')
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='images')
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    views = models.IntegerField(default=0) # Record how often a post is seen
+    tag = models.CharField(max_length=30, blank=True, null=True)
+    thumbnail_image = models.ImageField(upload_to='images')
+    vr_image = models.ImageField(upload_to='images')
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-        return self.name
+        return self.title
