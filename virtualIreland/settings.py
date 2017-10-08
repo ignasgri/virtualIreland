c0dbf7a80fe6ff2c375dc4447df15e6299773db9
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-08 10:53:12 +0100

    postgres added

--- virtualIreland/settings.py
+++ virtualIreland/settings.py
@@ -87,7 +87,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'virtualIreland.wsgi.application'
 
-
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
