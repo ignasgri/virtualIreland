4a1692415017dd6d121b7006992efe6056d41116
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-12 09:41:39 +0100

    Adding grid to page layout

--- static/js/swiper.js
+++ static/js/swiper.js
@@ -3,5 +3,27 @@
         slidesPerView: 3,
         paginationClickable: true,
         spaceBetween: 30,
-        freeMode: true
+        // freeMode: true,
+        breakpoints: {
+            1500: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
     });
