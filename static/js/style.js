296326324398ece81b2be8de0569a2acc05b3ffc
Ignas Grigaravicius <kickeris@gmail.com>
2017-10-19 22:19:33 +0100

    grid added for about details

--- static/js/style.js
+++ static/js/style.js
@@ -7,7 +7,7 @@
 
   $(document).ready(function(){
         $("#flip").click(function(){
-            $(this).html($(this).html() == 'Close window <span class="glyphicon glyphicon-triangle-top"></span>' ?
-             'Weather for all week <span class="glyphicon glyphicon-triangle-bottom"></span> ' : 'Close window <span class="glyphicon glyphicon-triangle-top"></span>');
+            $(this).html($(this).html() == '<span class="glyphicon glyphicon-triangle-top"></span>' ?
+             '<span class="glyphicon glyphicon-triangle-bottom"></span> ' : '<span class="glyphicon glyphicon-triangle-top"></span>');
         });
 });
